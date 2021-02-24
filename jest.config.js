935fbffeee4897d4f7219de09d758347fa4b1b5f
Rodrigo Santos <rodrigosantos9000@gmail.com>
2021-02-24 16:04:06 +0000

    add collectCoverageFrom property in jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 export default {
   coverageDirectory: 'coverage',
   coverageProvider: 'v8',
+  collectCoverageFrom: ['**/src/**/*.js'],
   testEnvironment: 'node',
   transform: {
     '.(js|jsx|ts|tsx)': '@sucrase/jest-plugin'
