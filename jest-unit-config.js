80b679a7dc0550026ea869ea8b65e721436b9464
Rodrigo Santos <62714522+RodrigoEnable@users.noreply.github.com>
2021-07-29 12:27:39 +0100

    Removed comments

--- jest-unit-config.js
+++ jest-unit-config.js
@@ -1,7 +1,5 @@
 import config from './jest.config.js'
 
-// qualquer arquivo (*) dentro de qualquer pasta (**) com a extensão spec.js, será um arquivo válido para essa configuração
-// o que fazemos aqui é adicionar ao objeto config a propriedade testMatch
 config.testMatch = ['**/*.spec.js']
 
 export default config
