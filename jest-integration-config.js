b37dd889d3647d65f292298b9bb95b5b29423faf
Rodrigo Santos <62714522+RodrigoEnable@users.noreply.github.com>
2021-07-29 12:28:10 +0100

    Removed comments

--- jest-integration-config.js
+++ jest-integration-config.js
@@ -1,7 +1,5 @@
 import config from './jest.config.js'
 
-// qualquer arquivo (*) dentro de qualquer pasta (**) com a extensão test.js, será um arquivo válido para essa configuração
-// o que fazemos aqui é adicionar ao objeto config a propriedade testMatch
 config.testMatch = ['**/*.test.js']
 
 export default config
